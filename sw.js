6b46a6f5582c70f9cba602504e5417691a195601
qs33ds-alt <qs33ds@gmail.com>
2026-08-18 02:44:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mi-rutina-v2';
+const CACHE='mi-rutina-v2-1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
